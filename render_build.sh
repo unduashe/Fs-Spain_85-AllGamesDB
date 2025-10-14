6420229f75471a93b484a4b14c8b61dd7c971c25
Sergio Moya <smoyamon@gmail.com>
2025-10-14 22:30:42 +0200

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
+# Build frontend
 cd src/front
 npm install
 npm run build
 cd ../..
 
-pipenv install
+# Instala pipenv manualmente (Render usa entorno Node por defecto)
+pip install pipenv
 
+# Instala dependencias de Python y ejecuta tareas
+pipenv install --deploy
 pipenv run flask db stamp head
-
 pipenv run migrate
-
 pipenv run upgrade
-
 pipenv run flask insert-game-tag-data
-
 pipenv run flask insert-game-data
